c5dd99d6ee296c4221861fac4e11c545b18bdfc3
Starling7 <129472418+Starling7@users.noreply.github.com>
2023-12-15 00:59:22 +0700

    Update app.sql

--- app.sql
+++ app.sql
@@ -7,16 +7,15 @@ create table tickets (
 	stadium_name text,
 	ticket_price text,
 	match_name text,
-	time_info time,
 	date_info date
 );
 
-insert into tickets (tribune_name, supporter_name, gender, stadium_name, ticket_price, match_name, time_info, date_info) 
+insert into tickets (tribune_name, supporter_name, gender, stadium_name, ticket_price, match_name, date_info) 
 values
-	('Economy', 'Agam', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '19:05', '2024-10-03'),
-	('VVIP', 'Jose', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '19:06', '2024-10-03'),
-	('VVIP', 'Malik', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '19:04', '2024-10-03'),
-	('VVIP', 'Yoze', 'female', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '19:20', '2024-10-03'),
-	('Economy', 'Ahmad', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '19:10', '2024-10-03'),
-	('Regular', 'Ammar', 'male', 'Jakarta International Stadium', 250000 , 'Indonesia vs Argentina', '19:11', '2024-10-03')
+	('Economy', 'Agam', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('VVIP', 'Jose', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina',  '2024-10-03'),
+	('VVIP', 'Malik', 'male', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('VVIP', 'Yoze', 'female', 'Jakarta International Stadium', 500000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('Economy', 'Ahmad', 'male', 'Jakarta International Stadium', 150000 , 'Indonesia vs Argentina', '2024-10-03'),
+	('Regular', 'Ammar', 'male', 'Jakarta International Stadium', 250000 , 'Indonesia vs Argentina', '2024-10-03')
 	;
